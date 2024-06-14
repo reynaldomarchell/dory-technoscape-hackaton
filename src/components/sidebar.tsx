a867a4cdf01907f72067083ea0d458b86826bc55
yoshikazuuu <febrianojerry@gmail.com>
2024-06-14 21:24:27 +0700

    feat: session and navbar hints

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,21 +1,41 @@
-import { Home, HomeIcon, UserIcon } from "lucide-react";
+"use client";
+
+import { Home, HomeIcon, PlusIcon, UserIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
 import Link from "next/link";
 import { histories, History } from "@/dummy/db";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export function Sidebar() {
+  const pathname = usePathname();
+
+  console.log(pathname);
+
   return (
     <aside className="flex-col gap-1 p-5 border-r w-[270px] h-[calc(100svh-3.5rem)] flex sticky top-0">
       <Link href="/">
-        <Button variant="ghost" className="flex justify-start gap-2 w-full">
+        <Button
+          variant="ghost"
+          className={cn(
+            "flex justify-start gap-2 w-full",
+            pathname == "/" && "bg-accent"
+          )}
+        >
           <HomeIcon />
           Dashboard
         </Button>
       </Link>
 
       <Link href="/profile">
-        <Button variant="ghost" className="flex justify-start gap-2 w-full">
+        <Button
+          variant="ghost"
+          className={cn(
+            "flex justify-start gap-2 w-full",
+            pathname == "/profile" && "bg-accent"
+          )}
+        >
           <UserIcon />
           Profile
         </Button>
@@ -27,17 +47,38 @@ export function Sidebar() {
         RIWAYAT TOPIK
       </p>
 
+      <Link href="/create-session">
+        <Button
+          className={cn("flex justify-start gap-2 w-full")}
+          variant={pathname == "/create-session" ? "outline" : "default"}
+        >
+          <PlusIcon />
+          Create New Session
+        </Button>
+      </Link>
+
       {histories.map((history, index) => (
-        <CardChatHistory key={index} history={history} />
+        <CardChatHistory key={index} history={history} pathname={pathname} />
       ))}
     </aside>
   );
 }
 
-function CardChatHistory({ history }: { history: History }) {
+function CardChatHistory({
+  history,
+  pathname,
+}: {
+  history: History;
+  pathname: string;
+}) {
   return (
     <Link href={`/history/${history.uuid}`}>
-      <div className="flex gap-2 hover:bg-accent rounded-lg p-4">
+      <div
+        className={cn(
+          "flex gap-2 hover:bg-accent rounded-lg p-4",
+          pathname == `/history/${history.uuid}` && "bg-accent"
+        )}
+      >
         <div className="flex flex-col  w-full">
           <p className="font-bold">{history.penanya}</p>
           <p className="truncate text-xs text-muted-foreground">
